6210158fdc62081cd41809c38bfb2cc289a58caf
JainilPrajapati07 <84768524+JainilPrajapati07@users.noreply.github.com>
2021-06-02 11:09:39 +0530

    Day 2

---
+++ Day 2/number.py
@@ -0,0 +1,13 @@
+n1 = 7
+
+print(n1,"is type of",type(n1))
+
+n2 = 7.5
+
+print(n2,"is type of",type(n2))
+print(n1,"is complex number ?",isinstance(7.5,int))
+
+n3 = 7 + 7j
+
+print(n3,"is type of",type(n3))
+print(n3,"is complex number ?",isinstance(7 + 7j,complex))
