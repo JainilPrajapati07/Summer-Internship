b748b05a765d024816b1551ccb7240b1c63c3a42
JainilPrajapati07 <84768524+JainilPrajapati07@users.noreply.github.com>
2021-06-02 11:12:27 +0530

    Day 2

---
+++ Day 2/tuple.py
@@ -0,0 +1,13 @@
+tuple1 = (7,7.5,77,'Jainil Prajapati',10,20,30,'Hello')
+
+print("tuple1 is ",tuple1)
+print(tuple1[0])
+print(tuple1[2:5])
+print(tuple1[2:])
+print(tuple1[:5])
+
+#error in tuple
+
+tuple1[0] = 70
+
+print("tuple1 is ",tuple1)
