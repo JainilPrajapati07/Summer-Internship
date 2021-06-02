df4d7691b8b683188b95b06d603637f6c51a041c
JainilPrajapati07 <84768524+JainilPrajapati07@users.noreply.github.com>
2021-06-02 10:59:06 +0530

    Day 2

---
+++ Day 2/variable.py
@@ -0,0 +1,35 @@
+#how to declare and use a variable 
+
+a=7
+b=7.7
+c='Jainil Prajapati'
+
+print("Value of a is ",a)
+print("Value of b is ",b)
+print("Value of c is ",c)
+
+#changing the value of a variable
+
+a=10
+b=20.5
+c='Jack'
+
+print("Value of a is ",a)
+print("Value of b is ",b)
+print("Value of c is ",c)
+
+#Assigning multiple values to multiple variables
+
+d,e,f=7,7.7,'Jainil Prajapati'
+
+print("Value of d is ",d)
+print("Value of e is ",e)
+print("Value of f is ",f)
+
+#Assign the same value to multiple variables
+
+g=h=i=77
+
+print("Value of g is ",g)
+print("Value of h is ",h)
+print("Value of i is ",i)
