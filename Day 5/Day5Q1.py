7b111f8eb5e1ed8f32472fdd1b33d1a5b04f2dae
Jainil Prajapati <84768524+JainilPrajapati07@users.noreply.github.com>
2021-06-11 09:52:43 +0530

    Day 5

---
+++ Day 5/Day5Q1.py
@@ -0,0 +1,11 @@
+class MyClass:
+    def func1(self):
+        print('Have a Good Day !')
+
+    def func2(self, name):
+        print("Name is : "+name)
+
+myc = MyClass()
+
+myc.func1()
+myc.func2("Jainil Prajapati")
